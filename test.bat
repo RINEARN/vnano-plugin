e12ff71ed702cc1fec70bf7ba7f2da7d83a8579c
RINEARN <rinearn@gmail.com>
2020-07-20 23:46:01 +0900

    Add: test script for MathElementaryXnci1Plugin

---
+++ test.bat
@@ -0,0 +1,25 @@
+@echo off
+echo.
+echo - Vnano Standard Plug-ins Test -
+echo.
+echo --------------------------------------------------
+echo.
+
+
+echo [ MathElementaryXnci1PluginTest.vnano (accelerator disabled) ]
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
+if %errorlevel% neq 0 exit /b 1
+
+echo.
+
+echo [ MathElementaryXnci1PluginTest.vnano (accelerator enabled) ]
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
+if %errorlevel% neq 0 exit /b 1
+
+
+echo.
+echo --------------------------------------------------
+echo.
+echo All tests have been completed successfully.
+exit /b 0
+
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+echo ""
+echo " - Vnano Standard Plug-ins Test -"
+echo ""
+echo "--------------------------------------------------"
+echo ""
+
+
+echo "[ MathElementaryXnci1PluginTest.vnano (accelerator disabled) ]"
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
+
+echo ""
+
+echo "[ MathElementaryXnci1PluginTest.vnano (accelerator enabled) ]"
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
+
+
+echo ""
+echo "--------------------------------------------------"
+echo ""
+echo "All tests have been completed successfully."
+

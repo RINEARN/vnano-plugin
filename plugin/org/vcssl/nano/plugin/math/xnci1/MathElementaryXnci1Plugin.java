27e8de3fcc9c7f08864c8f32dc7e45cd0d4544ce
RINEARN <rinearn@gmail.com>
2021-02-01 08:51:15 +0900

    Add: fast implementations of math plug-ins for scalar args
    Existing math plug-ins supports both of scalar and array args,
    but their overheads for accessing data of args
    (through ArrayDataAccessorInterface1)
    are relatively heavy,
    so they might be bottlenecks for numerical computation scripts
    calling math functions frequently with scalar args.
    
    Thus, we have added implementations of math plug-ins
    which handle only scalar args.
    Their overheads for accessing data of args
    (through Float64ScalarDataAccessorInterface1)
    are lighter than existing implementations.
    
    After this commit, when math functions are called,
    generally existing math plug-ins will be invoked, but
    if all args are scalar, new plug-ins with names "FastScalar..."
    will be invoked for accelerating execution speeds.

--- plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1Plugin.java
+++ plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1Plugin.java
@@ -19,6 +19,20 @@ import org.vcssl.nano.plugin.math.xfci1.LnXfci1Plugin;
 import org.vcssl.nano.plugin.math.xfci1.Log10Xfci1Plugin;
 import org.vcssl.nano.plugin.math.xfci1.PowXfci1Plugin;
 import org.vcssl.nano.plugin.math.xfci1.RadXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarAbsXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarAcosXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarAsinXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarAtanXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarCosXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarDegXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarExpXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarLnXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarLog10Xfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarPowXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarRadXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarSinXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarSqrtXfci1Plugin;
+import org.vcssl.nano.plugin.math.xfci1.FastScalarTanXfci1Plugin;
 import org.vcssl.nano.plugin.math.xfci1.SinXfci1Plugin;
 import org.vcssl.nano.plugin.math.xfci1.SqrtXfci1Plugin;
 import org.vcssl.nano.plugin.math.xfci1.TanXfci1Plugin;
@@ -41,6 +55,10 @@ public class MathElementaryXnci1Plugin implements ExternalNamespaceConnectorInte
 	@Override
 	public ExternalFunctionConnectorInterface1[] getFunctions() {
 		List<ExternalFunctionConnectorInterface1> functionList = new LinkedList<ExternalFunctionConnectorInterface1>();
+
+		// General implementations, supporting scalar & array (any dim) arguments
+		// スカラ含む任意次元配列の引数に対応した汎用実装
+		// （プラグイン仕様書に記載の全機能は、一応これらの汎用実装のみで満たしている）
 		functionList.add(new RadXfci1Plugin());
 		functionList.add(new DegXfci1Plugin());
 		functionList.add(new SinXfci1Plugin());
@@ -55,6 +73,25 @@ public class MathElementaryXnci1Plugin implements ExternalNamespaceConnectorInte
 		functionList.add(new PowXfci1Plugin());
 		functionList.add(new ExpXfci1Plugin());
 		functionList.add(new AbsXfci1Plugin());
+
+		// Fast implementations, supporting only scalar arguments
+		// スカラ引数のみに対応した高速実装
+		// （機能的には上の汎用実装のみで全部揃っているが、以下を接続しておけば引数がスカラの場合に高速化される）
+		functionList.add(new FastScalarRadXfci1Plugin());
+		functionList.add(new FastScalarDegXfci1Plugin());
+		functionList.add(new FastScalarSinXfci1Plugin());
+		functionList.add(new FastScalarCosXfci1Plugin());
+		functionList.add(new FastScalarTanXfci1Plugin());
+		functionList.add(new FastScalarAsinXfci1Plugin());
+		functionList.add(new FastScalarAcosXfci1Plugin());
+		functionList.add(new FastScalarAtanXfci1Plugin());
+		functionList.add(new FastScalarSqrtXfci1Plugin());
+		functionList.add(new FastScalarLnXfci1Plugin());
+		functionList.add(new FastScalarLog10Xfci1Plugin());
+		functionList.add(new FastScalarPowXfci1Plugin());
+		functionList.add(new FastScalarExpXfci1Plugin());
+		functionList.add(new FastScalarAbsXfci1Plugin());
+
 		return functionList.toArray(new ExternalFunctionConnectorInterface1[0]);
 	}
 

dd75cacc4efa13c594d2765a15c9e205b6a784a9
RINEARN <rinearn@gmail.com>
2021-03-02 14:10:23 +0900

    Update: scripts for testing
    On recent versions of the Vnano,
    the command line option "--optLevel"
    for specifying the optimization level
    is available.
    
    Hence,
    for testing plug-ins under the every
    optimization level currently supported,
    we have updated following test-scripts:
    
    * test.sh
    * test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,8 @@
 @echo off
+setlocal
+
+set maxOptLevel=3
+
 echo.
 echo - Vnano Standard Plug-ins Test -
 echo.
@@ -6,54 +10,63 @@ echo --------------------------------------------------
 echo.
 
 
-echo [ SystemEnvironmentXnci1PluginTest.vnano (accelerator disabled) ]
+
+echo [ SystemEnvironmentXnci1PluginTest.vnano ^(accelerator disabled^) ]
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemEnvironmentXnci1PluginTest.vnano
 if %errorlevel% neq 0 exit /b 1
-
 echo.
 
-echo [ SystemEnvironmentXnci1PluginTest.vnano (accelerator enabled) ]
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemEnvironmentXnci1PluginTest.vnano
+for /l %%i in (1, 1, %maxOptLevel%) do (
+echo [ SystemEnvironmentXnci1PluginTest.vnano ^(accelerator enabled, optimization leve %%i^) ]
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel %%i ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemEnvironmentXnci1PluginTest.vnano
 if %errorlevel% neq 0 exit /b 1
-
 echo.
+)
 
-echo [ SystemDataTypeXnci1PluginTest.vnano (accelerator disabled) ]
+
+
+echo [ SystemDataTypeXnci1PluginTest.vnano ^(accelerator disabled^) ]
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1PluginTest.vnano
 if %errorlevel% neq 0 exit /b 1
-
 echo.
 
-echo [ SystemDataTypeXnci1PluginTest.vnano (accelerator enabled) ]
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1PluginTest.vnano
+for /l %%i in (1, 1, %maxOptLevel%) do (
+echo [ SystemDataTypeXnci1PluginTest.vnano ^(accelerator enabled, optimization level %%i^) ]
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel %%i ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1PluginTest.vnano
 if %errorlevel% neq 0 exit /b 1
-
 echo.
+)
 
-echo [ MathElementaryXnci1PluginTest.vnano (accelerator disabled) ]
+
+
+echo [ MathElementaryXnci1PluginTest.vnano ^(accelerator disabled^) ]
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
 if %errorlevel% neq 0 exit /b 1
-
 echo.
 
-echo [ MathElementaryXnci1PluginTest.vnano (accelerator enabled) ]
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
+for /l %%i in (1, 1, %maxOptLevel%) do (
+echo [ MathElementaryXnci1PluginTest.vnano ^(accelerator enabled, optimization level %%i^) ]
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel %%i ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
 if %errorlevel% neq 0 exit /b 1
-
 echo.
+)
 
-echo [ MathStatisticalXnci1PluginTest.vnano (accelerator disabled) ]
+
+
+echo [ MathStatisticalXnci1PluginTest.vnano ^(accelerator disabled^) ]
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/math/xnci1/MathStatisticalXnci1PluginTest.vnano 
 if %errorlevel% neq 0 exit /b 1
-
 echo.
 
-echo [ MathStatisticalXnci1PluginTest.vnano (accelerator enabled) ]
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/math/xnci1/MathStatisticalXnci1PluginTest.vnano 
+for /l %%i in (1, 1, %maxOptLevel%) do (
+echo [ MathStatisticalXnci1PluginTest.vnano ^(accelerator enabled, optimization level %%i^) ]
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel %%i ./plugin/org/vcssl/nano/plugin/math/xnci1/MathStatisticalXnci1PluginTest.vnano 
 if %errorlevel% neq 0 exit /b 1
-
-
 echo.
+)
+
+
+
 echo --------------------------------------------------
 echo.
 echo All tests have been completed successfully.
--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -eu
+
+maxOptLevel=3
+
 echo ""
 echo " - Vnano Standard Plug-ins Test -"
 echo ""
@@ -7,46 +10,59 @@ echo "--------------------------------------------------"
 echo ""
 
 
+
 echo "[ SystemEnvironmentXnci1PluginTest.vnano (accelerator disabled) ]"
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemEnvironmentXnci1PluginTest.vnano
-
 echo ""
 
-echo "[ SystemEnvironmentXnci1PluginTest.vnano (accelerator enabled) ]"
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemEnvironmentXnci1PluginTest.vnano
-
+for i in `seq 0 1 $maxOptLevel`
+do
+echo "[ SystemEnvironmentXnci1PluginTest.vnano (accelerator enabled, optimization level $i) ]"
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel $i ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemEnvironmentXnci1PluginTest.vnano
 echo ""
+done
+
+
 
 echo "[ SystemDataTypeXnci1PluginTest.vnano (accelerator disabled) ]"
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1PluginTest.vnano
-
 echo ""
 
-echo "[ SystemDataTypeXnci1PluginTest.vnano (accelerator enabled) ]"
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1PluginTest.vnano
-
+for i in `seq 0 1 $maxOptLevel`
+do
+echo "[ SystemDataTypeXnci1PluginTest.vnano (accelerator enabled, optimization level $i) ]"
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel $i ./plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1PluginTest.vnano
 echo ""
+done
+
+
 
 echo "[ MathElementaryXnci1PluginTest.vnano (accelerator disabled) ]"
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
-
 echo ""
 
-echo "[ MathElementaryXnci1PluginTest.vnano (accelerator enabled) ]"
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
-
+for i in `seq 0 1 $maxOptLevel`
+do
+echo "[ MathElementaryXnci1PluginTest.vnano (accelerator enabled, optimization level $i) ]"
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel $i ./plugin/org/vcssl/nano/plugin/math/xnci1/MathElementaryXnci1PluginTest.vnano
 echo ""
+done
+
+
 
 echo "[ MathStatisticalXnci1PluginTest.vnano (accelerator disabled) ]"
 java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator false ./plugin/org/vcssl/nano/plugin/math/xnci1/MathStatisticalXnci1PluginTest.vnano
-
 echo ""
 
-echo "[ MathStatisticalXnci1PluginTest.vnano (accelerator enabled) ]"
-java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true ./plugin/org/vcssl/nano/plugin/math/xnci1/MathStatisticalXnci1PluginTest.vnano
-
-
+for i in `seq 0 1 $maxOptLevel`
+do
+echo "[ MathStatisticalXnci1PluginTest.vnano (accelerator enabled, optimization level $i) ]"
+java -jar Vnano.jar --pluginList ./plugin/VnanoPluginList_AllStandards.txt --accelerator true --optLevel $i ./plugin/org/vcssl/nano/plugin/math/xnci1/MathStatisticalXnci1PluginTest.vnano
 echo ""
+done
+
+
+
 echo "--------------------------------------------------"
 echo ""
 echo "All tests have been completed successfully."

0ca0b556cf9120e48d8ae98e6875302c5e0d168f
RINEARN <commit@rinearn.com>
2022-11-30 22:19:57 +0900

    Add: new variable: HALF_TO_EVEN_SIGNIF

--- plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1Plugin.java
+++ plugin/org/vcssl/nano/plugin/system/xnci1/SystemDataTypeXnci1Plugin.java
@@ -36,6 +36,7 @@ import org.vcssl.nano.plugin.system.xvci1.HalfUpSignifXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.HalfDownXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.HalfDownSignifXvci1Plugin;
 import org.vcssl.nano.plugin.system.xvci1.HalfToEvenXvci1Plugin;
+import org.vcssl.nano.plugin.system.xvci1.HalfToEvenSignifXvci1Plugin;
 
 public class SystemDataTypeXnci1Plugin implements ExternalNamespaceConnectorInterface1 {
 
@@ -80,6 +81,7 @@ public class SystemDataTypeXnci1Plugin implements ExternalNamespaceConnectorInte
 		variableList.add(new HalfDownXvci1Plugin());
 		variableList.add(new HalfDownSignifXvci1Plugin());
 		variableList.add(new HalfToEvenXvci1Plugin());
+		variableList.add(new HalfToEvenSignifXvci1Plugin());
 		return variableList.toArray(new ExternalVariableConnectorInterface1[0]);
 	}
 
